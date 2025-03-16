4eb340e6458ce94c34eac2d32c4668fd5b04dcdd
ghr0 <514659991@qq.com>
2025-03-16 15:01:40 +0800

    Delete index14 directory

--- index14/script.js
+++
@@ -1,7 +0,0 @@
-        function rotateAvatar() {
-            const avatar = document.getElementById('avatar');
-            avatar.classList.add('rotate');
-            setTimeout(() => {
-                avatar.classList.remove('rotate');
-            }, 800);
-        }
